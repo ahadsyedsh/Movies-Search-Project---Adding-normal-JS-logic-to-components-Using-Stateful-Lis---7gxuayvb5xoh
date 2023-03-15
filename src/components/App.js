e9bd7a3ac9c952c351db62a6b032098ff3f5b38f
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-16 03:01:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,23 @@ const App = () => {
     const [inputValue, setInputValue] = useState("");
     const [results, setResult] = useState(null);
 
-    function handleClick(e) {}
+    function handleClick(e) {
+        e.preventDefault();
+        const searchByRegex = new RegExp(inputValue, "i");
 
-    function handleChange(e) {}
+        const filteredMovie = movies.filter((movie)=>{
+            return searchByRegex.test(movie.title)
+        })
+        
+        setResult(filteredMovie)
+    }
 
+    function handleChange(e) {
+        const input = e.target.value
+        setInputValue(input);
+    }
+
+    // console.log(results)
     return (
         <div id="main">
             <form id="form">
@@ -17,17 +30,20 @@ const App = () => {
                     type="text"
                     onChange={handleChange}
                     value={inputValue}
-                    id="movie-input"
-                ></input>
+                    id="movie-input"> 
+                </input>
                 <button id="button" onClick={handleClick}>
                     Search
                 </button>
             </form>
             <div id="result">
                 {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
+                    results.map((movie) => {
+                        
+                        return <div className="movie" key={movie.id}>{movie.title}</div>
+                        
+                    })
+                }
             </div>
         </div>
     );
